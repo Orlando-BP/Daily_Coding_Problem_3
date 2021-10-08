c0ee94135178b5d5830f1054f94672083535bc47
Orlando <orlando.barba7980@alumnos.udg.mx>
2021-10-08 13:21:16 -0500

    Solution 1

---
+++ Node.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Daily_Coding_Problem_3
+{
+    class Node
+    {
+        string val;
+        Node left;
+        Node right;
+        public Node(string val,Node left= null, Node right = null) 
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+        public string get_val()
+        {
+            return val;
+        }
+        public Node get_left()
+        {
+            return left;
+        }
+        public Node get_right()
+        {
+            return right;
+        }
+        public void set_left(Node left)
+        {
+            this.left = left;
+        }
+        public void set_right(Node right)
+        {
+            this.right = right;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Daily_Coding_Problem_3
+{
+    class Program
+    {
+        const string NullNode = "?";
+        static void Main(string[] args)
+        {
+            /* Given the root to a binary tree, 
+             * implement serialize(root), 
+             * which serializes the tree into a string, 
+             * and deserialize(s), 
+             * which deserializes the string back into the tree.
+             * (Read the file README.md for more information)
+             */
+            Node node = new Node("root", new Node("left", new Node("left.left")), new Node("right"));
+            if (deserialize(serialize(node)).get_left().get_left().get_val() == "left.left")
+                Console.WriteLine("True");
+            else
+                Console.WriteLine("False");
+        }
+        public static string serialize(Node node)
+        {
+            if (node == null)
+                return NullNode + '/';
+
+            StringBuilder SB = new StringBuilder();
+
+            SB.Append(node.get_val() + '/');
+            SB.Append(serialize(node.get_left()));
+            SB.Append(serialize(node.get_right()));
+
+            return SB.ToString();
+        }
+        public static Node deserialize(string serialization)
+        {
+            string[] nodes = serialization.Split('/', StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+            var queue = new Queue<string>(nodes);
+            var node = LoadNode(queue);
+
+            return node;
+        }
+
+        private static Node LoadNode(Queue<string> nodes)
+        {
+            if (nodes.Peek() != null)
+            {
+                string nextNode = nodes.Dequeue();
+                if (nextNode == NullNode)
+                    return null;
+
+                Node node = new Node(nextNode);
+                node.set_left(LoadNode(nodes));
+                node.set_right(LoadNode(nodes));
+                return node;
+            }
+            return null;
+        }
+    }
+}
